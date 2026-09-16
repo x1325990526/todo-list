218bbb180603cb41d8eb5059fe203b648879bcb8
Nova <64618129+x1325990526@users.noreply.github.com>
2026-09-16 09:32:12 +0800

    chore: remove restaurant page template files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-// index.js
+
 
 import "./styles.css";
 
-import displayHome from "./home.js";
-import displayMenu from "./menu.js";
-import displayAbout from "./about.js";
-
-displayHome();
-
-const nav = document.querySelector("nav");
-const content =document.querySelector("#content");
-
-nav.addEventListener("click", (e) => {
-    const selected = e.target
-    if(!selected.matches("button")) return;
-    if(selected.classList.contains("home")) {
-        content.textContent = "";
-        displayHome();
-    }
-    if(selected.classList.contains("menu")) {
-        content.textContent = "";
-        displayMenu();
-    }
-    if(selected.classList.contains("about")) {
-        content.textContent = "";
-        displayAbout();
-    }
-});
